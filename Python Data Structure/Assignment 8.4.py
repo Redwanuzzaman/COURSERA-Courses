be7e660d832428e0d4d547d81442022432c8875e
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-06-08 02:58:04 +0600

    Create Assignment 8.4.py

---
+++ Python Data Structure/Assignment 8.4.py
@@ -0,0 +1,12 @@
+fname = input("Enter file name: ")
+if len(fname) == 0:
+    fname = 'romeo.txt'   
+fh = open(fname)
+lst = list()
+for line in fh:
+    for i in line.split():
+        if not i in lst:
+            lst.append(i)
+        
+lst.sort()
+print(lst)
