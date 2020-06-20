01d910eed2b1c5e711052af8b924c702da36165b
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-06-20 20:49:23 +0600

    Update Assignment 5.2.py

--- Programming for Everybody (Getting Started with Python)/Assignment 5.2.py
+++ Programming for Everybody (Getting Started with Python)/Assignment 5.2.py
@@ -1,19 +1,18 @@
-num = 0
 largest = -1
 smallest = None
 while True:
     num = input("Enter a number: ")
-    if num == "done":
+    if num == "done" :
         break
-    try:
+    try :
         numb = int(num)
     except:
         print('Invalid input')
-    if smallest is None:
+    if smallest is None :
         smallest = numb
-    elif numb < smallest:
+    elif numb < smallest :
         smallest = numb
-    elif numb > largest:
+    elif numb > largest :
         largest = numb
 print("Maximum is", largest)
 print("Minimum is", smallest)
