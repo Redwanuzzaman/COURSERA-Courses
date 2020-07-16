3f9a21035d06ffdf6ae2871db1e8f1e3a539b0dc
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-07-16 16:04:55 +0600

    Create Practice Quiz: Conditionals.py

---
+++ Python Crash Course By Google/Practice Quiz: Conditionals.py
@@ -0,0 +1,39 @@
+# 1
+
+def greeting(name):
+  if name == "Taylor":
+    return "Welcome back Taylor!"
+  else:
+    return "Hello there, " + name
+
+print(greeting("Taylor"))
+print(greeting("John"))
+
+
+# 2
+
+if number > 11: 
+  print(0)
+elif number != 10:
+  print(1)
+elif number >= 20 or number < 12:
+  print(2)
+else:
+  print(3)
+# answer = 2
+
+
+# 3
+
+import math
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks = math.ceil(filesize / block_size)
+    
+    return full_blocks * block_size
+
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
